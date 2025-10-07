6aadc51914aa186eaf05ae4765ac73a2cf05a5f8
Kenzo Trindade <kenzo.trindade@gmail.com>
2025-10-07 11:01:33 +0200

    day.c - Edit

--- day.c
+++ day.c
@@ -1,42 +1,51 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main() {
+int main(){
     int hour;
     int day;
     int month;
+    const char* hour_fact;
+    const char* month_fact;
+    const char* day_fact;
+    bool weekly = false;
 
-    printf("Veuillez saisir l'heure : ");
+    printf("Entrez l'heure : ");
     scanf("%d", &hour);
 
-    printf("Veuillez saisir le jour : ");
+    printf("Entrez le jour de la semaine en chiffre (0 étant lundi) : ");
     scanf("%d", &day);
 
-    printf("Veuillez saisir le mois : ");
+    printf("Entrez le mois en chiffre (0 étant janvier) : ");
     scanf("%d", &month);
 
-    if (6<hour && hour<=12) {
-        printf("C'est le matin à %d\n", hour);
-    } else if  (12<hour && hour<=19) {
-        printf("C'est le après-midi à %d\n", hour);
-    } else if (20<=hour && hour<=6) {
-        printf("C'est le soir à %d\n", hour);
-    } else {
-        printf("Entrez une heure valide");
+    if (6 <= hour && hour <= 12){
+           hour_fact = "matin";
+    } else if (13 <= hour && hour <= 19){
+        hour_fact = "après-midi";
+    } else if (20 <= hour || hour <= 5){
+        hour_fact = "soir";
     }
 
-    if (11<=month && month<=2) {
-        printf("C'est l'hiver en %d\n", month);
-    } else if (3<=month && month<=4) {
-        printf("C'est le Printemps en %d\n", month);
-    } else if (5<=month && month<=7) {
-        printf("C'est l'été en %d\n", month);
-    } else if (8<=month && month<=10) {
-        printf("C'est l'automne en %d\n", month);
+    if ( 11<= month || month <= 2){
+        month_fact = "d'hiver" ;
+    } else if ( 3<= month || month <= 4){
+        month_fact = "de printemps" ;
+    } else if ( 5<= month || month <= 7){
+        month_fact = "d'été" ;
+    } else if ( 8<= month || month <= 10){
+        month_fact = "d'automne" ;
     }
 
+    if ( day == 5 || day == 6 ){
+        weekly = true;
+        day_fact = "le week-end";
+    }
 
-    if (5<=day && day<=6) {
-        printf("C'est le week-end aujourd'hui :  %d\n", day);
+    if ( weekly == true ) {
+        printf("C'est un %s %s %s",hour_fact, month_fact, day_fact);
+    } else if ( weekly == false ) {
+        printf("C'est un %s %s ",hour_fact, month_fact);
     }
     
 }
