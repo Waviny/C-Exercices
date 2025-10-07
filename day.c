eeedba1dd332cc8fb49d037e75d7e9f6ceb75ef0
Kenzo Trindade <kenzo.trindade@gmail.com>
2025-10-07 09:58:08 +0200

    Calculator - EditDay.c

--- day.c
+++ day.c
@@ -14,30 +14,30 @@ int main() {
     printf("Veuillez saisir le mois : ");
     scanf("%d", &month);
 
-    if (6<hour<=12) {
+    if (6<hour && hour<=12) {
         printf("C'est le matin à %d\n", hour);
-    } else if  (12<hour<=13) {
+    } else if  (12<hour && hour<=13) {
         printf("C'est le midi à %d\n", hour);
-    } else if (13<hour<=19) {
+    } else if (13<hour && hour<=19) {
         printf("C'est le après-midi à %d\n", hour);
-    } else if (20<=hour<=6) {
+    } else if (20<=hour && hour<=6) {
         printf("C'est le soir à %d\n", hour);
     } else {
         printf("Entrez une heure valide");
     }
 
-    if (11<=month<=2) {
+    if (11<=month && month<=2) {
         printf("C'est l'hiver en %d\n", month);
-    } else if (3<=month<=4) {
+    } else if (3<=month && month<=4) {
         printf("C'est le Printemps en %d\n", month);
-    } else if (5<=month<=7) {
+    } else if (5<=month && month<=7) {
         printf("C'est l'été en %d\n", month);
-    } else if (8<=month<=10) {
+    } else if (8<=month && month<=10) {
         printf("C'est l'automne en %d\n", month);
     }
 
 
-    if (5<=day<=6) {
+    if (5<=day && day<=6) {
         printf("C'est le week-end aujourd'hui :  %d\n", day);
     }
     
