10b661fb2b173c907e4cff4a23ed4983603ec9d0
Kenzo Trindade <kenzo.trindade@gmail.com>
2025-10-09 10:48:51 +0200

    EditMalloc

--- malloc.c
+++ malloc.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void dynamic_tab(char *tab) {
+void dynamic_tab(int *tab) {
     tab = malloc(26 * sizeof(char));
-    printf("%s\n", tab);
     for (int i = 0; i < 26; i++) {
-        tab[i] = 'a' + i;
+        tab[i] = 97 + i;
+        printf("%c", tab[i]);
     }
-    
-    printf("Voici le contenue du tableau : %s\n", tab);
     free(tab);
 }
 
-char tab[26];
+int tab[26];
 
 int main() {
     dynamic_tab(tab);
